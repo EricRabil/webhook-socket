c45c6b181a06f120d2593f272f439f07d18a3fa3
EricRabil <ericjrabil@gmail.com>
2018-07-24 16:19:24 -0400

    delete config.ts

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-export default class Configuration {
-    static websocketToken: string = "ravioli-ravioli-give-me-the-formuoli";
-    static whitelist: string[] = [];
-    static port: number = 9090;
-
-    static twitterSecret?: string;
-}
