8672d1a2074244c2f86a7e61304bab5ac5ccfc8b
EricRabil <ericjrabil@gmail.com>
2018-07-24 16:18:22 -0400

    ignore src/config.ts

---
+++ src/config.example.ts
@@ -0,0 +1,7 @@
+export default class Configuration {
+    static websocketToken: string = "ravioli-ravioli-give-me-the-formuoli";
+    static whitelist: string[] = [];
+    static port: number = 9090;
+
+    static twitterSecret?: string;
+}
